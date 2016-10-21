3cd9ced31edbcbd4d74e899d764f4d5d120988c0
modern12 d <modern12@o2.pl>
2016-10-21 10:02:47 +0200

    first comm
    first com

---
+++ Assets/GroundGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGenerator : MonoBehaviour {
+
+    public int mapWidth = 32;
+    public int mapHeight = 32;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+    public void BuildGround()
+    {
+        for (int i = 0; i < mapHeight; i++)
+        {
+            for (int j = 0; j < mapHeight; j++)
+            {
+
+            }
+        }
+    }
+
+}
